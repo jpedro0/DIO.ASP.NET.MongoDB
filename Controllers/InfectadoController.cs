81b9ff278bd2ac311033a9088836dd3e009712fc
JPedro Brito <jpedro0brito@gmail.com>
2021-03-29 15:44:01 -0400

    Criando regra do projeto adicioando controller e classes para utilizar o mongodb no asp.net

---
+++ Controllers/InfectadoController.cs
@@ -0,0 +1,32 @@
+﻿using DIO.ASP.NET.MongoDB.Data.Collections;
+using DIO.ASP.NET.MongoDB.Models;
+using Microsoft.AspNetCore.Mvc;
+using MongoDB.Driver;
+using System.Threading.Tasks;
+
+namespace DIO.ASP.NET.MongoDB.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class InfectadoController : ControllerBase
+    {
+        private readonly IMongoCollection<Infectado> _infectadosCollection;
+
+        public InfectadoController(Data.MongoDB mongoDB) => 
+            (_infectadosCollection) = (mongoDB.DB.GetCollection<Infectado>(typeof(Infectado).Name.ToLower()));
+
+        [HttpGet]
+        public async Task<IActionResult> ObterInfectadosAsync() => 
+            Ok((await _infectadosCollection.FindAsync(Builders<Infectado>.Filter.Empty)).ToList());
+
+        [HttpPost]
+        public async Task<IActionResult> SalvarInfectadoAsync([FromBody] InfectadoDto dto)
+        {
+            var infectado = new Infectado(dto.DataNascimento, dto.Sexo, dto.Latitude, dto.Longitude);
+
+            await _infectadosCollection.InsertOneAsync(infectado);
+
+            return Created("Infectado/ObterInfectados", "Infectado adicionado com sucesso");
+        }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -22,18 +22,16 @@ namespace DIO.ASP.NET.MongoDB
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
 
-            services.AddControllers();
+            services.AddSingleton<Data.MongoDB>();
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "DIO.ASP.NET.MongoDB", Version = "v1" });
             });
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
@@ -44,8 +42,6 @@ namespace DIO.ASP.NET.MongoDB
             }
 
             app.UseRouting();
-
-            app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
             {
